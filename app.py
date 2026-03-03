da5eee6001feb4ee1aca670b9b174e01e4177b10
S-Davies-28 <sam489577@gmail.com>
2026-03-03 16:08:28 +0000

    Added UK category

--- app.py
+++ app.py
@@ -8,16 +8,21 @@ soup = BeautifulSoup(tech_page_to_scrape.text, "html.parser")
 world_page_to_scrape = requests.get("https://www.bbc.co.uk/news/world")
 soup2 = BeautifulSoup(world_page_to_scrape.text, "html.parser")
 
+uk_page_to_scrape = requests.get("https://www.bbc.co.uk/news/uk")
+soup3 = BeautifulSoup(uk_page_to_scrape.text, "html.parser")
+
 tech_headings = soup.select('a[href*="/news/"] span[aria-hidden="false"]')
 world_headings = soup2.select('a[href*="/news/"] span[aria-hidden="false"]')
+uk_headings = soup3.select('a[href*="/news/"] span[aria-hidden="false"]')
 
 file = open("scraped_quotes.csv", "w")
 writer = csv.writer(file)
 
-writer.writerow(["Technology News", "World News"])
+writer.writerow(["Technology News", "World News", "UK News"])
 
-for tech, world in zip(tech_headings[:7], world_headings[:7]):
+for tech, world, uk in zip(tech_headings[:7], world_headings[:7], uk_headings[:7]):
     print(tech.text)
     print(world.text)
-    writer.writerow([tech.text, world.text])
+    print(uk.text)
+    writer.writerow([tech.text, world.text, uk.text])
 file.close()
